854cacbaab2eb4d9c1d5035db2f6559736ee82b7
JackCurzon <Jack@172-16-18-65.DYNAPOOL.NYU.EDU>
2015-10-27 14:19:12 -0400

    first assignment

---
+++ Stars/sketch.js
@@ -0,0 +1,171 @@
+
+  function setup() {
+  createCanvas(1400, 800);
+  background(15,0,30);
+ 
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+
+  
+}
+
+
+function draw() {
+  background(15,0,30);
+  fill(255,216,10);
+  ellipse(1300, 100, 80, 80);
+  fill(200, 200, 200)
+  ellipse(1250, 100, 5, 5)
+  fill(240,207,129);
+  ellipse(1230, 100, 8, 8);
+  fill(58, 99, 240);
+  ellipse(1210, 100, 8, 8);
+  fill(245, 127, 36);
+  ellipse(1190, 100, 5, 5)
+  fill(240, 206, 204);
+  ellipse(1170, 100, 15, 15)
+  
+   fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  fill(200, 200, 50);
+  var s = stars(0, 1400, 0, 800);
+  ellipse(random(0, 1400), random(0, 800), 5, 5);
+  
+  fill(250, 150, 3)
+  triangle(mouseX, mouseY+50, mouseX-25, mouseY+125, mouseX+25, mouseY+125)
+  fill(153,153,153)
+  triangle(mouseX, mouseY, mouseX-75, mouseY+75, mouseX+75, mouseY+75)
+  fill(23,45,201)
+  ellipse(mouseX, mouseY, 50, 50)
+  fill(153,153,153)
+  rect(mouseX-26, mouseY, 51, 100)
+  fill(0,0,0)
+  text("USS", mouseX-13, mouseY+30)
+  text("Dubois", mouseX-17, mouseY+45)
+  
+  
+  
+  }
+
+function stars(min, max, Q)
+{
+  
+  var value = 0.;
+  
+  for(var i = 0; i < Q; i++) // i = i + 1
+  {
+    value+=random(min, max);
+  }
+ 
+  value = value / Q;
+  
+  return(value);
+  
+}
